0a088d4c3af8dedd65659d1e5b6aebd465c48af3
nuevoRON <hronaldjosue@gmail.com>
2022-10-08 01:06:06 -0600

    tabla bitacora

---
+++ vistas/contenidos/bitacora-view.php
@@ -0,0 +1,80 @@
+<div class="full-box page-header">
+	<h3 class="text-center">
+		<i class="fas fa-book-reader"></i> BITACORA
+	</h3>
+    
+
+</div>
+
+
+
+<div class="container-fluid">
+	<div class="table-responsive">
+    <caption>Aqui Encontraremos Toda la Informacion de Ingresos, Actualizaciones y  Eliminacion de Datos</caption>
+		<table class="table table-dark table-sm">
+			<thead>
+				<tr class="text-center roboto-medium">
+					<th>#</th>
+					<th>FECHA</th>
+					<th>USUARIO</th>
+					<th>OBJETO</th>
+					<th>ACCION</th>
+					<th>DESCRIPCION</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr class="text-center" >
+					<td>1</td>
+					<td>01/23/2022</td>
+					<td>NOMBRE</td>
+					<td>INVENTARIO</td>
+					<td>INGRESO NUEVO INVENTARIO</td>
+					<td>SE AGREGO CAFE CON SABOR A MENTA</td>
+				</tr>
+                <tr class="text-center" >
+					<td>2</td>
+					<td>01/23/2022</td>
+					<td>NOMBRE</td>
+					<td>INVENTARIO</td>
+					<td>INGRESO NUEVO INVENTARIO</td>
+					<td>SE AGREGO CAFE CON SABOR A COCOA</td>
+				</tr><tr class="text-center" >
+					<td>3</td>
+					<td>01/23/2022</td>
+					<td>NOMBRE</td>
+					<td>INVENTARIO</td>
+					<td>INGRESO NUEVO INVENTARIO</td>
+					<td>SE AGREGO CAFE CON SABOR A VAINIA</td>
+				</tr><tr class="text-center" >
+					<td>4</td>
+					<td>01/23/2022</td>
+					<td>NOMBRE</td>
+					<td>INVENTARIO</td>
+					<td>ACTUALIZACION DE INVENTARIO</td>
+					<td>SE ACTULIZO INVENTARIO 003</td>
+				</tr><tr class="text-center" >
+					<td>5</td>
+					<td>01/23/2022</td>
+					<td>NOMBRE</td>
+					<td>INVENTARIO</td>
+					<td>ELIMINACION DE INVENTARIO</td>
+					<td>SE ELIMINO CAFE CON SABOR A COCOA</td>
+				</tr>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+	<nav aria-label="Page navigation example">
+		<ul class="pagination justify-content-center">
+			<li class="page-item disabled">
+				<a class="page-link" href="#" tabindex="-1">Previous</a>
+			</li>
+			<li class="page-item"><a class="page-link" href="#">1</a></li>
+			<li class="page-item"><a class="page-link" href="#">2</a></li>
+			<li class="page-item"><a class="page-link" href="#">3</a></li>
+			<li class="page-item">
+				<a class="page-link" href="#">Next</a>
+			</li>
+		</ul>
+	</nav>
+</div>
