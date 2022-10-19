ea93e040ca081839964bf378cc8f47a4c4675901
maguilera4323 <113657563+maguilera4323@users.noreply.github.com>
2022-10-18 20:04:38 -0600

    codigo de boton ver contraseña login

--- vistas/js/funciones.js
+++ vistas/js/funciones.js
@@ -1,3 +1,14 @@
+function mostrarContrasena(){
+  let tipo = document.getElementById("clave");
+ if(tipo.type == "password"){
+     tipo.type = "text";
+     $('.icon-clave').removeClass('fas fa-eye-slash').addClass('fas fa-eye');
+ }else{
+     tipo.type = "password";
+     $('.icon-clave').removeClass('fas fa-eye').addClass('fas fa-eye-slash');
+ }
+}
+
 function mostrarContrasenaNueva(){
     let tipo = document.getElementById("clave_new");
    if(tipo.type == "password"){
