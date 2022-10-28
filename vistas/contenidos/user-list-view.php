77a36be716a42c3a6a4ed039f8b6cb0c1ed21b79
ronalprogra <hronaldjosue@hotmail.com>
2022-10-28 09:27:18 -0600

    borrado

--- vistas/contenidos/user-list-view.php
+++ vistas/contenidos/user-list-view.php
@@ -26,9 +26,7 @@
 		<li>
 			<a class="active" href="<?php echo SERVERURL; ?>user-list/"><i class="fas fa-clipboard-list fa-fw"></i> &nbsp; LISTA DE USUARIOS</a>
 		</li>
-		<li>
-			<a href="<?php echo SERVERURL; ?>user-search/"><i class="fas fa-search fa-fw"></i> &nbsp; BUSCAR USUARIO</a>
-		</li>
+
 	</ul>	
 </div>
 
@@ -43,8 +41,7 @@ if(isset($_GET['enviar'])){
 
 	if (isset($_GET['busqueda']))
 	{
-		$where="WHERE TBL_usuarios.usuario LIKE'%".$busqueda."%' OR nombre_usuario  LIKE'%".$busqueda."%'
-    OR telefono  LIKE'%".$busqueda."%'";
+		$where="WHERE TBL_usuarios.usuario LIKE'%".$busqueda."%' OR nombre_usuario  LIKE'%".$busqueda."%'";
 	}
   
 }
@@ -145,66 +142,7 @@ if($dato -> num_rows >0){
 
 
 
-<!-- <div class="table-responsive">
 
-	<form action="buscar_usuario.php" method="get" class="" >
-		<input type="text" name="busqueda" id="busqueda" placeholder="Usuario a Buscar">
-		<input type="submit" value="buscar" class="btn_search">
-	</form>
-
-	<table class="table table-dark table-sm">
-		<thead>
-			<tr class="text-center roboto-medium">
-				<th>#</th>
-				<th>USUARIO</th>
-				<th>NOMBRE</th>
-				<th>ESTADO</th>
-				<th>ROL</th>
-				<th>CORREO</th>
-				<th>CREADO POR</th>
-				<th>ACTUALIZAR</th>
-				<th>ELIMINAR</th>
-			</tr>
-		</thead>
-		<?php
-			include ("./cone.php");
-			$sql="SELECT u.id_usuario, u.usuario, u.nombre_usuario, u.estado_usuario, u.correo_electronico,
-			u.creado_por, r.rol FROM TBL_usuarios u
-			inner JOIN TBL_ms_roles r ON u.id_rol = r.id_rol";
-			$result=mysqli_query($conexion,$sql);
-			while($mostrar=mysqli_fetch_assoc($result)){			
-		?>
-				<tbody>
-					<tr class="text-center" >
-						<td><?php echo $mostrar['id_usuario']?></td>
-						<td><?php echo $mostrar['usuario']?></td>
-						<td><?php echo $mostrar['nombre_usuario']?></td>
-						<td><?php echo $mostrar['estado_usuario']?></td>
-						<td><?php echo $mostrar['rol']?></td>
-						<td><?php echo $mostrar['correo_electronico']?></td>
-						<td><?php echo $mostrar['creado_por']?></td>
-						<td>
-						<a href="<?php echo SERVERURL; ?>user-update/<?php echo $mostrar['id_usuario']?>" class="btn btn-success">
-									<i class="fas fa-sync-alt"></i>	
-							</a>
-						</td>
-						<td>
-							<form class="FormularioAjax" action="<?php echo SERVERURL; ?>ajax/usuarioAjax.php" method="POST" data-form="delete" autocomplete="off">
-							<input type="hidden" pattern="" class="form-control" name="id_usuario_del" value="<?php echo $mostrar['id_usuario'] ?>">
-							<input type="hidden" pattern="" class="form-control" name="usuario_del" value="<?php echo $mostrar['usuario'] ?>">	
-							<button type="submit" class="btn btn-warning">
-										<i class="far fa-trash-alt"></i>
-								</button>
-							</form>
-						</td>
-					</tr>
-				</tbody>
-		<?php
-			}
-			
-		?>
-		
-	</table> -->
 		
 
 </div>
