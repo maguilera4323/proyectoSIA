909fa72532a53ae9853059670c9ff2ee0a14fe50
SYANETH <silvia.salinas@unah.hn>
2022-10-30 22:05:48 -0600

    Funcion de actulizar y eliminar proveedores

--- controladores/proveedorControlador.php
+++ controladores/proveedorControlador.php
@@ -69,6 +69,7 @@ class proveedorControlador extends proveedorModelo
 	/*--------- Controlador actualizar usuario ---------*/
 	public function actualizar_proveedor_controlador()
 	{	
+		$id_actualizar=mainModel::limpiar_cadena($_POST['id_actualizacion']);
 		$Nombre=mainModel::limpiar_cadena($_POST['nombre_proveedor_actu']);
 		$Rtn=mainModel::limpiar_cadena($_POST['rtn_proveedor_actu']);
 		$Telefono=mainModel::limpiar_cadena($_POST['telefono_proveedor_actu']);
@@ -88,7 +89,7 @@ class proveedorControlador extends proveedorModelo
 
 
 			/*== Verificando integridad de los datos ==*/
-			if(mainModel::verificar_datos("[A-ZÁÉÍÓÚÑ]{1,30}",$Nombre)){
+			if(mainModel::verificar_datos("[A-ZÁÉÍÓÚÑ ]{1,30}",$Nombre)){
 				$alerta=[
 					"Alerta"=>"simple",
 					"Titulo"=>"Ocurrió un error inesperado",
@@ -110,21 +111,42 @@ class proveedorControlador extends proveedorModelo
 				exit();
 			}
 			
-			if(mainModel::verificar_datos("[0-9]{1,14}",$Telefono)){
+			if(mainModel::verificar_datos("[0-9]{1,20}",$Telefono)){
 				$alerta=[
 					"Alerta"=>"simple",
 					"Titulo"=>"Ocurrió un error inesperado",
-					"Texto"=>"El RTN no coincide con el formato solicitado",
+					"Texto"=>"El TELEFONO no coincide con el formato solicitado",
+					"Tipo"=>"error"
+				];
+				echo json_encode($alerta);
+				exit();
+			}
+
+			if(mainModel::verificar_datos("[a-z@_0-9.]{1,30}",$Correo)){
+				$alerta=[
+					"Alerta"=>"simple",
+					"Titulo"=>"Ocurrió un error inesperado",
+					"Texto"=>"El CORREO no coincide con el formato solicitado",
+					"Tipo"=>"error"
+				];
+				echo json_encode($alerta);
+				exit();
+			}
+
+			if(mainModel::verificar_datos("[A-Za-zÑñ0-9 .,]{1,100}",$Direccion)){
+				$alerta=[
+					"Alerta"=>"simple",
+					"Titulo"=>"Ocurrió un error inesperado",
+					"Texto"=>"La DIRECCION no coincide con el formato solicitado",
 					"Tipo"=>"error"
 				];
 				echo json_encode($alerta);
 				exit();
 			}
 		
-
 	
 			/*== ACTUALIZAR PROVEEDOR ==*/
-		$datos_usuario_actu=
+		$datos_proveedor_actu=
 			[
 				"nombre"=>$Nombre,
 				"rtn"=>$Rtn,
@@ -175,83 +197,58 @@ class proveedorControlador extends proveedorModelo
 
 	
 
-		//se verifica que el usuario a bloquear o inactivar no sea el usuario administrador
-		if($Usuario=="ADMIN"){
-			$alerta=[
-				"Alerta"=>"simple",
-				"Titulo"=>"Ocurrió un error inesperado",
-				"Texto"=>"El usuario ADMIN no puede ser eliminado",
-				"Tipo"=>"error"
-			];
-			echo json_encode($alerta);
-			exit();
-		}
+		//funcion para eliminar un proveedor
+		public function eliminarProveedor()
+		{
+			$id=mainModel::limpiar_cadena(($_POST['id_proveedor_del']));
+			$Proveedor=mainModel::limpiar_cadena(($_POST['proveedor_del']));
+			$array=array();
+			$valor='';
+
+		
 
 		//verifica que el usuario si exista en el sistema
-		$check_usuario=mainModel::ejecutar_consulta_simple("SELECT id_usuario FROM TBL_usuarios 
-		WHERE id_usuario='$id'");
-		if($check_usuario->rowCount()<=0){
+		$check_proveedor=mainModel::ejecutar_consulta_simple("SELECT id_Proveedores FROM TBL_Proveedores
+		WHERE id_Proveedores='$id'");
+		if($check_proveedor->rowCount()<=0){
 			$alerta=[
 				"Alerta"=>"simple",
 				"Titulo"=>"Ha ocurrido un error",
-				"Texto"=>"El usuario seleccionado no existe",
+				"Texto"=>"El Proveedor seleccionado no existe",
 				"Tipo"=>"error"
 			];
 			echo json_encode($alerta);
 			exit();
 		}
 
-		//se verifica el valor del primer ingreso debido a que un usuario que ha sido creado y no ha ingresado al sistema
-		//aun no ha configurado sus preguntas de seguridad
-		$verificarPrimerIngreso=mainModel::ejecutar_consulta_simple("SELECT * FROM TBL_usuarios 
-		WHERE id_usuario='$id'");
-		foreach($verificarPrimerIngreso as $fila){
-			$array['valor']=$fila['primer_ingreso'];
-		}
-
-		//si el usuario si ha ingresado al sistema se procede a inactivar para evitar errores de la bd
-		if($array['valor']==1){
-			$eliminarUsuario=usuarioModelo::eliminar_usuario_modelo("inactivar",$id);
-				if($eliminarUsuario->rowCount()==1){
+		
+		$eliminarproveedor=proveedorModelo::eliminar_proveedor_modelo("borrar",$id);
+			if($eliminarproveedor->rowCount()==1){
 				$alerta=[
 					"Alerta"=>"recargar",
-					"Titulo"=>"Usuario inactivado",
-					"Texto"=>"El usuario fue inactivado, ya que existen registros conectados con el mismo.",
+					"Titulo"=>"Usuario Borrado",
+					"Texto"=>"El Proveedor fue borrado",
 					"Tipo"=>"success"
 				];
 			}else{
 				$alerta=[
 					"Alerta"=>"simple",
 					"Titulo"=>"Ha ocurrido un error",
-					"Texto"=>"El usuario no pudo ser inactivado",
+					"Texto"=>"El proveedor no pudo ser borrado",
 					"Tipo"=>"error"
 				];
 			}
 			echo json_encode($alerta);
+		$datos_bitacora = [
+			"id_objeto" => 0,
+			"fecha" => date('Y-m-d H:i:s'),
+			"id_usuario" => $_SESSION['id_login'],
+			"accion" => "Usuario inactivado",
+			"descripcion" => "El usuario ".$_SESSION['usuario_login']." inactivó un usuario del sistema"
+		];
+		Bitacora::guardar_bitacora($datos_bitacora);
 			exit();
 
-			//si el usuario no ha ingresado al sistema por primera vez se elimina el registro
-		}else if($array['valor']==0){
-			$eliminarUsuario=usuarioModelo::eliminar_usuario_modelo("eliminar",$id);
-				if($eliminarUsuario->rowCount()==1){
-				$alerta=[
-					"Alerta"=>"recargar",
-					"Titulo"=>"Usuario eliminado",
-					"Texto"=>"El usuario fue eliminado del sistema",
-					"Tipo"=>"success"
-				];
-			}else{
-				$alerta=[
-					"Alerta"=>"simple",
-					"Titulo"=>"Ha ocurrido un error",
-					"Texto"=>"El usuario no pudo ser eliminado",
-					"Tipo"=>"error"
-				];
-			}
-			echo json_encode($alerta);
-			exit();
-		}
+			
 	}
-
-
 }
