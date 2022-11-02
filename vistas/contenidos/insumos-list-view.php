9b4ce7426f7d34fbe89579b2645e35f202f59154
maguilera4323 <maynoraguileraosorto@gmail.com>
2022-11-02 12:01:17 -0600

    avance modulo insumos

---
+++ vistas/contenidos/insumos-list-view.php
@@ -0,0 +1,144 @@
+<?php
+	require_once "./pruebabitacora.php";
+
+	$datos_bitacora = [
+		"id_objeto" => 0,
+		"fecha" => date('Y-m-d H:i:s'),
+		"id_usuario" => $_SESSION['id_login'],
+		"accion" => "Cambio de vista",
+		"descripcion" => "El usuario ".$_SESSION['usuario_login']." entró a la vista de Insumos"
+	];
+	Bitacora::guardar_bitacora($datos_bitacora); 
+?>
+
+<div class="full-box page-header">
+	<h3 class="text-left">
+		<i class="fas fa-clipboard-list fa-fw"></i> &nbsp; INSUMOS
+	</h3>
+
+</div>
+
+<div class="container-fluid">
+	<ul class="full-box list-unstyled page-nav-tabs">
+		<li>
+			<a href="<?php echo SERVERURL; ?>user-new/"><i class="fas fa-plus fa-fw"></i> &nbsp; NUEVO INSUMO</a>
+		</li>
+		<li>
+			<a class="active" href="<?php echo SERVERURL; ?>user-list/"><i class="fas fa-clipboard-list fa-fw"></i> &nbsp; INVENTARIO DISPONIBLE</a>
+		</li>
+		<li>
+			<a class="active" href="<?php echo SERVERURL; ?>user-list/"><i class="fas fa-clipboard-list fa-fw"></i> &nbsp; MOVIMIENTOS DE INVENTARIO</a>
+		</li>
+	</ul>	
+</div>
+
+
+<?php
+include ("./cone.php");
+$where="";
+
+if(isset($_GET['enviar'])){
+  $busqueda = $_GET['busqueda'];
+
+
+	if (isset($_GET['busqueda']))
+	{
+		$where="WHERE TBL_insumos.nom_insumo LIKE'%".$busqueda."%'";
+	}
+  
+}
+
+
+?>
+
+			</form>
+      <div class="container-fluid">
+  <form class="d-flex">
+      <input class="form-control me-2 light-table-filter" data-table="table_id" type="text" 
+      placeholder="Buscar insumo">
+      <hr>
+      </form>
+  </div>
+
+  <br>
+
+ 
+      <table class="table table-striped table-dark table_id " id="tblDatos">
+                         <thead>    
+                         <tr>
+                        <th>ID</th>
+                        <th>NOMBRE</th>
+                        <th>CATEGORIA</th>
+                        <th>CANTIDAD MAXIMA</th>
+                        <th>CANTIDAD MINIMA</th>
+                        <th>UNIDAD DE MEDIDA</th>
+						<th>ACTUALIZAR</th>
+						<th>ELIMINAR</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+				<?php
+
+include ("./cone.php");              
+$SQL="SELECT i.id_insumos, i.nom_insumo, c.nom_categoria, i.cant_max, i.cant_min,i.unidad_medida FROM TBL_insumos i
+inner JOIN TBL_categoria_produ c ON c.id_categoria = i.id_categoria
+$where";
+$dato = mysqli_query($conexion, $SQL);
+
+if($dato -> num_rows >0){
+    while($fila=mysqli_fetch_array($dato)){
+    
+?>
+<tr>
+<td><?php echo $fila['id_insumos']; ?></td>
+<td><?php echo $fila['nom_insumo']; ?></td>
+<td><?php echo $fila['nom_categoria']; ?></td>
+<td><?php echo $fila['cant_max']; ?></td>
+<td><?php echo $fila['cant_min']; ?></td>
+<td><?php echo $fila['unidad_medida']; ?></td>
+<td>
+	<a href="<?php echo SERVERURL; ?>user-update/<?php echo $fila['id_insumos']?>" class="btn btn-success">
+		<i class="fas fa-sync-alt"></i>	
+	</a>
+</td>
+<td>
+	<form class="FormularioAjax" action="<?php echo SERVERURL; ?>ajax/usuarioAjax.php" method="POST" data-form="delete" autocomplete="off">
+	<input type="hidden" pattern="" class="form-control" name="id_usuario_del" value="<?php echo $fila['id_insumos'] ?>">
+	<input type="hidden" pattern="" class="form-control" name="usuario_del" value="<?php echo $fila['nom_insumo'] ?>">	
+	<button type="submit" class="btn btn-warning">
+		<i class="far fa-trash-alt"></i>
+	</button>
+	</form>
+</td>
+</tr>
+
+
+<?php
+}
+}else{
+
+    ?>
+    <tr class="text-center">
+    <td colspan="16">No existen registros</td>
+    </tr>
+
+    
+    <?php
+    
+}
+
+?>
+
+
+	</body>
+  </table>
+  <div id="paginador" class=""></div>	
+
+<div class="container-fluid">
+
+
+
+
+		
+
+</div>
---
+++ vistas/contenidos/insumos-new-view.php
@@ -0,0 +1,68 @@
+<div class="full-box page-header">
+	<h3 class="text-left">
+		<i class="fas fa-plus fa-fw"></i> &nbsp; AGREGAR PROVEEDOR
+	</h3>
+
+</div>
+
+<div class="container-fluid">
+	<ul class="full-box list-unstyled page-nav-tabs">
+		<li>
+			<a class="active" href="<?php echo SERVERURL; ?>proveedor-new/"><i class="fas fa-plus fa-fw"></i> &nbsp; AGREGAR PROVEEDOR</a>
+		</li>
+		<li>
+			<a href="<?php echo SERVERURL; ?>proveedor-list/"><i class="fas fa-clipboard-list fa-fw"></i> &nbsp; LISTA DE PROVEEDORES</a>
+		</li>
+		<li>
+			<a href="<?php echo SERVERURL; ?>proveedor-search/"><i class="fas fa-search fa-fw"></i> &nbsp; BUSCAR PROVEEDOR</a>
+		</li>
+	</ul>	
+</div>
+
+<div class="container-fluid">
+	<form action="<?php echo SERVERURL; ?>ajax/proveedorAjax.php" class="form-neon FormularioAjax" method="POST" data-form="save" autocomplete="off">
+		<fieldset>
+			<legend><i class="fas fa-user"></i> &nbsp; Información básica</legend>
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-12 col-md-6">
+						<div class="form-group">
+							<label for="cliente_dni" class="bmd-label-floating">Nombre</label>
+							<input type="text" class="form-control" name="nombre_proveedor_nuevo" id="cliente_dni" maxlength="27">
+						</div>
+					</div>
+					<div class="col-12 col-md-6">
+						<div class="form-group">
+							<label for="cliente_nombre" class="bmd-label-floating">RTN</label>
+							<input type="text" class="form-control" name="rtn_proveedor_nuevo" id="cliente_nombre" maxlength="40">
+						</div>
+					</div>
+					<div class="col-12 col-md-4">
+						<div class="form-group">
+							<label for="cliente_apellido" class="bmd-label-floating">Telefono</label>
+							<input type="text" class="form-control" name="telefono_proveedor_nuevo" id="cliente_apellido" maxlength="40">
+						</div>
+					</div>
+					<div class="col-12 col-md-4">
+						<div class="form-group">
+							<label for="cliente_telefono" class="bmd-label-floating">Correo</label>
+							<input type="text" class="form-control" name="correo_proveedor_nuevo" id="cliente_telefono" maxlength="20">
+						</div>
+					</div>
+					<div class="col-12 col-md-4">
+						<div class="form-group">
+							<label for="cliente_direccion" class="bmd-label-floating">Dirección</label>
+							<input type="text" class="form-control" name="direccion_proveedor_nuevo" id="cliente_direccion" maxlength="150">
+						</div>
+					</div>
+				</div>
+			</div>
+		</fieldset>
+		<br><br><br>
+		<p class="text-center" style="margin-top: 40px;">
+			<button type="reset" class="btn btn-raised btn-secondary btn-sm"><i class="fas fa-paint-roller"></i> &nbsp; LIMPIAR</button>
+			&nbsp; &nbsp;
+			<button type="submit" class="btn btn-raised btn-info btn-sm"><i class="far fa-save"></i> &nbsp; GUARDAR</button>
+		</p>
+	</form>
+</div>
